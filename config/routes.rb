8ba24226fc914b58026a1599cb75e256ec17f681
WilliamMena <williammena1991@gmail.com>
2017-03-02 22:33:06 -0500

    Add/modify Users and Sessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'session#home'
+  root 'sessions#welcome'
+
+  get '/home' => 'users#home'
+  resources :users, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
