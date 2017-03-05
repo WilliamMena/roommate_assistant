50b82cbfda1eae15d695d24fc3bbc6391656e0b7
WilliamMena <williammena1991@gmail.com>
2017-03-04 20:49:37 -0500

    Add create List functionality

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,10 +5,30 @@ class ListsController < ApplicationController
   end
 
   def edit
-    @list = List.new
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = current_user.lists.build(list_params)
+    @list.save
+    redirect_to user_list_path(current_user.id, @list.id)
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to user_list_path(current_user.id, @list.id)
   end
 
   def show
     @list = List.find(params[:id])
+  end
+
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
   end
 end
