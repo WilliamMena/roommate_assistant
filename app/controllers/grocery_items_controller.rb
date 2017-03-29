4bbddb8c21f88916f54dddacbc6a20c6bbd1547c
WilliamMena <williammena1991@gmail.com>
2017-03-29 09:36:45 -0400

    Add edit functionality to grocery_items

--- app/controllers/grocery_items_controller.rb
+++ app/controllers/grocery_items_controller.rb
@@ -9,6 +9,17 @@ class GroceryItemsController < ApplicationController
     redirect_to user_list_path(list.user.id, list.id)
   end
 
+  def edit
+    @grocery_item = GroceryItem.find(params[:id])
+  end
+
+  def update
+    grocery_item = GroceryItem.find(params[:id])
+    grocery_item.update(grocery_params)
+    grocery_item.save
+    redirect_to user_list_path(grocery_item.list.user.id, grocery_item.list.id)
+  end
+
   def buy
     gi = GroceryItem.find(params[:id])
     gi.update(bought: true)
@@ -28,6 +39,9 @@ class GroceryItemsController < ApplicationController
     flash.alert = "Grocery Deleted"
   end
 
+  def grocery_params
+    params.require(:grocery_item).permit(:item)
+  end
 
 
 end
