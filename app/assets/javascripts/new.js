a0bb8d4ee8f2491629bf4fcd3e37af1be3e4329a
WilliamMena <williammena1991@gmail.com>
2017-07-18 23:18:18 -0400

    Added functioning handlebars to /lists/new

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,7 +1,10 @@
-
-
 function showLists() {
-  debugger
-  var source   = $("#entry-template").html();
+  var source   = $("#lists-template").html();
   var template = Handlebars.compile(source);
+  $.get("/lists.json", function(data) {
+    var lists = data
+    if (lists.length > 0) {
+      $("#lists")[0].innerHTML = template(lists)
+    }
+  })
 }
