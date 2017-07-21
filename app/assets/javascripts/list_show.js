2f900cf59e342a9cc4a6e388f557641cbc9ead81
WilliamMena <williammena1991@gmail.com>
2017-07-20 21:19:05 -0400

    Now able to retrieve list item object that was called inside listener

--- app/assets/javascripts/list_show.js
+++ app/assets/javascripts/list_show.js
@@ -2,23 +2,50 @@ $(document).ready(function() {
   attachListeners();
 })
 
+
+// function findId(group) {
+//   debugger
+//   return function(group) {
+//     return group.id === group
+//   }
+// }
+
+function findId(id, group) {
+  for (var i=0; i < group.length; i++) {
+    if (group[i].id == id) {
+      return group[i];
+    }
+  }
+}
+
 function attachListeners() {
-  // add a json view to the show pages so when trying to update each item for buying, the data is accessable. 
+  // add a json view to the show pages so when trying to update each item for buying, the data is accessable.
   var values = {grocery_item: {
     bought: false
   }}
-  $("#statusChange_1").on("click", function(e) {
-    e.preventDefault()
-    console.log("Hello")
 
-    var itemSave = $.ajax({
-      type: "PATCH",
-      url: '/grocery_items/1',
-      data: values
-    });
-    itemSave.done(function(data) {
-      console.log("Hello")
+  $(".statusChange").on("click", function(e) {
+    e.preventDefault()
+
+    var id = this.attributes["data-item"].value
+
+    var listJson = $.get(window.location.pathname+'.json')
+
+    listJson.done(function(data) {
+      var object = findId(id, data.grocery_items)
+      debugger
     })
+
+
+    var id = this.attributes["data-item"].value
+    // var itemSave = $.ajax({
+    //   type: "PATCH",
+    //   url: `/grocery_items/${id}`,
+    //   data: values
+    // });
+    // itemSave.done(function(data) {
+    //   console.log("Hello")
+    // })
   })
 }
 
